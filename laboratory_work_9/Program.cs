bf439a2ff4514e303e90d537723174a63096ceb0
Vademado <VademadoIU8@yandex.ru>
2024-11-17 01:59:58 +0300

    add laboratory work 9

---
+++ laboratory_work_9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+#nullable enable
+
+namespace laboratory_work_9
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            StockDataFetcher stockDataFetcher = new StockDataFetcher();
+            await stockDataFetcher.GetData();
+        }
+    }
+}
