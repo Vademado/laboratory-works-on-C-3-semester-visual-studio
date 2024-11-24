b1fd67dc1649d05110f0fa0e80f8e70416339ff5
Vademado <VademadoIU8@yandex.ru>
2024-11-24 23:39:49 +0300

    add laboratory work 10

---
+++ laboratory_work_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+
+namespace laboratory_work_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TickerContext db = new TickerContext())
+            {
+                //db.Database.EnsureDeleted();
+                //db.Database.EnsureCreated();
+            }
+        }
+    }
+}
---
+++ laboratory_work_10/TickerContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace laboratory_work_10
+{
+    internal class TickerContext : DbContext
+    {
+        public virtual DbSet<Ticker> Tickers { get; set; } = null;
+        public virtual DbSet<TodayCondition> Companies { get; set; } = null;
+        public virtual DbSet<Price> Prices { get; set; } = null;
+
+        public TickerContext() { }
+
+        public TickerContext(DbContextOptions<TickerContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=localhost,1443;Initial Catalog=TickerDataBase;User ID=sa;Password=yourStrong(!)Password");
+            }
+        }
+    }
+}
